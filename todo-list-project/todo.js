6eda2a5fa8f6b7ab29c8f88207f088d5b8ade989
ohhheyyyy <ashley.hartt@gmail.com>
2016-04-11 12:33:31 -0400

    Version 1.0 of to do app

---
+++ todo-list-project/todo.js
@@ -0,0 +1,19 @@
+// It should have a place to store todos
+
+var todos = [];
+
+// It should have a way to display todos
+
+console.log("My todos: ", todos);
+
+// It should have a way to add new todos
+
+todos.push(todo);
+
+// It should have a way to change a todo
+
+todos[0] = "edited todo item";
+
+// It should have a way to delete a todo
+
+todos.splice(0, 1);
